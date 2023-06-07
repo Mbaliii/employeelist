02f2ca73aff54183c0222263f87b8158bec06a24
Mbali <mbalikhumalo408@gmail.com>
2023-06-08 00:45:55 +0200

    l

--- src/Home.js
+++ src/Home.js
@@ -1,82 +1,29 @@
-// import { useEffect, useState } from "react";
-// import { Link} from "react-router-dom";
-
-// const Home = () => {
-// 	// const usenavigate = useNavigate();
-// 	const [employeelist, listupdate] = useState(null);
-// 	const [displayname, displaynameupdate] = useState('');
-// 	useEffect(() => {
-
-
-// 		let jwttoken = sessionStorage.getItem('jwttoken');
-// 		fetch("https://localhost:44308/Customer", {
-// 			headers: {
-// 				'Authentication': 'bearer ' + jwttoken
-// 			}
-// 		}).then((res) => {
-// 			return res.json();
-// 		}).then((resp) => {
-// 			listupdate(resp);
-// 		}).catch((err) => {
-// 			console.log(err.messsage)
-// 		});
-
-// 	}, []);
-// 	return (
-// 		<div>
-// 			<div className="header">
-// 				<Link to={'/'}>Home</Link>
-// 				<Link style={{ float: 'right' }} to={'/login'}>Login</Link>
-// 			</div>
-// 			<center><h1 className="text-center">Welcome to AXZ Corp</h1></center>
-// 			{/* <table className="table table-bordered">
-// 				<thead>
-// 					<tr>
-// 						<td>ID</td>
-// 						<td>Image</td>
-// 						<td>Name</td>
-// 						<td>Surname</td>
-// 						<td>Email</td>
-// 						<td>Position</td>
-// 						<td>Action</td>
-// 					</tr>
-// 				</thead>
-// 				<tbody>
-// 					{employeelist &&
-// 						listupdate.map(item => (
-// 							<tr key={item.id}>
-// 								<td>{item.id}</td>
-// 								<td>{item.name}</td>
-// 								<td>{item.email}</td>
-// 								<td>{item.creditLimit}</td>
-// 							</tr>
-
-// 						))
-// 					}
-// 				</tbody>
-
-// 			</table> */}
-
-// 		</div>
-// 	);
-// }
-
-// export default Home;
-
-
 
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
 	const [data, setData] = useState([])
+	const navigate = useNavigate();
 	useEffect(() => {
-		axios.get(" http://localhost:5000/user") 
-		.then(res => setData(res.data))
-		.catch(err => console.log(err));
+		axios.get(" http://localhost:5000/user")
+			.then(res => setData(res.data))
+			.catch(err => console.log(err));
 
-	}, [])
+	}, []) 
+
+
+	const handleDelete = (id) => {
+		const confirm = window.confirm("Would you wish to delete? ");
+		if (confirm) {
+			axios.delete('http://localhost:5000/user' + id)
+				.then(res => {
+					navigate('/')
+				}).catch(err => console.log(err));
+		}
+
+	}
 	return (
 		<div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
 			<h1>
@@ -108,8 +55,8 @@ function Home() {
 									<td>{d.contact}</td>
 									<td>
 										<Link to={'/read/{$d.id}'} className="  btn btn-sa btn-info me-2">Read</Link>
-										<button className="  btn btn-sa btn-primary me-2">Edit</button>
-										<button className="  btn btn-sm btn-danger">Delete</button>
+										<Link to={'/update/{$d.id}'} className="  btn btn-sa btn-primary me-2">Edit</Link>
+										<button onClick={e => handleDelete(d.id)} className="  btn btn-sm btn-danger">Delete</button>
 									</td>
 								</tr>
 							))
--- src/Update.js
+++ src/Update.js
@@ -1,8 +1,90 @@
-import React from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function Update() {
-    return(
-        <div>Update</div>
+    // const [data, setData] = useState([])
+    const { id } = useParams();
+    const [values, setValues] = useState({
+        name: "",
+        surname: "",
+        bio: "",
+        dateofbirth: "",
+        position: "",
+        contact: "",
+        email: ""
+
+    })
+    const navigate = useNavigate(); 
+    useEffect(() => {
+        axios.get(" http://localhost:5000/user/" + id)
+            .then(res => {
+                setValues(res.data); 
+            })
+            .catch(err => console.log(err));
+    }, [])
+
+    const handleUpdate = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/user/' +id, values)
+            .then(res => {
+                console.log(res);
+                navigate('/')
+            })
+            .catch(err => console.log(err));
+
+    }
+    return (
+
+        <div>
+            <div className="d-flex w-100 justify-content-center align-items-center bg-light">
+                <div className="w-50 border bg-white chadow px-5 pt-3 pb-5 rounded">
+                    <h1>Update User Employee</h1>
+                    <form onSubmit={handleUpdate}>
+                        <div className="mb-2">
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" name="name" className="form-control" placeholder="Enter you name" value={values.name}
+                                onChange={e => setValues({ ...values, name: e.target.value })} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="surname">Surname:</label>
+                            <input type="text" name="Surname" className="form-control" placeholder="Enter you surname" value={values.surname}
+                                onChange={e => setValues({ ...values, surname: e.target.value })} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="bio">Bio:</label>
+                            <input type="text" name="bio" className="form-control" placeholder="Enter you bio" value={values.bio}
+                                onChange={e => setValues({ ...values, bio: e.target.value })} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="dob">Date of birth:</label>
+                            <input type="text" name="dob" className="form-control" placeholder="Enter you DOB" value={values.dob}
+                                onChange={e => setValues({ ...values, dob: e.target.value })} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="position">Position:</label>
+                            <input type="text" name="position" className="form-control" placeholder="Enter you position" value={values.position}
+                                onChange={e => setValues({ ...values, position: e.target.value })} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="email">Email:</label>
+                            <input type="text" name="email" className="form-control" placeholder="Enter Email" value={values.email}
+                                onChange={e => setValues({ ...values, email: e.target.value })} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="email">Phone:</label>
+                            <input type="texy" name="phone" className="form-control" placeholder="Enter phone number" value={values.contact}
+                                onChange={e => setValues({ ...values, phone: e.target.value })} />
+                        </div>
+                        <button className="btn btn-success">Update</button>
+                        <Link to="/" className="btn btn-primary ms-3">Back</Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
