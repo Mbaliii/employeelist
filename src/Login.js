a70d0d3fca122f28a18ba7eb979ce616c39903e0
Mbali <mbalikhumalo408@gmail.com>
2023-06-07 13:01:23 +0200

    audit

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const Login = () => {
@@ -12,42 +12,110 @@ const Login = () => {
 	const [contact, contactupdate] = useState('')
 
 
-	const ProceedLogin = (e) => {
-		e.preventDefault()
-		if (validate()) {
+	const navigate = useNavigate();
 
-		}
 
-	}
-	const validate = () => {
+	const Isvalidate = () => {
 		// let result = true;
 		let isproceed = true;
-		let errormessage = "Enter the below commands!!! "
+		let errormessage = "Enter the commands below!!! "
 		if (name === '' || name === null) {
 			isproceed = false;
 			errormessage += 'Name'
-			// result=false;
+			// result = false;
 			// toast.warning('Name ');
 		}
-		if (surname === '' || surname === null){
+		if (surname === '' || surname === null) {
 			isproceed = false;
 			errormessage += 'Surname, '
-			// result=false;
+			// result = false;
 			// toast.warning('Surname ')
 		}
-		if (bio === '' || bio === null){
+		if (bio === '' || bio === null) {
 			isproceed = false;
 			errormessage += 'Bio, ';
-			// result=false;
+			// result = false;
 			// toast.warning('Bio ')
+		}
+		if (dateofbirth === '' || dateofbirth === null) {
+			isproceed = false;
+			errormessage += 'DateOfBirth, ';
+			// result = false;
+			// toast.warning('Date of birth')
+		}
+		// if (email === '' || email === null) {
+		// 	isproceed = false;
+		// 	errormessage+= 'email, ';
+		// 	// result = false;
+		// 	// toast.warning('Email')
+		// }
+
+		if (position === '' || position === null) {
+			isproceed = false;
+			errormessage += 'Position, ';
+			// result = false;
+			// toast.warning('Position')
+		}
+
+		if (contact === '' || contact === null) {
+			isproceed = false;
+			errormessage += 'Contact, ';
+			// result = false;
+			// toast.warning('Contact')
+		}
+
+		if (!isproceed) {
+			toast.warning(errormessage);
+		} else {
+			if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
+
+			} else {
+				isproceed = false;
+				toast.warning('Please enter a valid email ')
+			}
 		}
 		return isproceed;
 
 	}
+
+	const handlesubmit = (e) => {
+		e.preventDefault();
+		let regobj = { name, surname, email, bio, dateofbirth, position, contact }
+		if (Isvalidate()) {
+			fetch("http://localhost:5000/user", {
+				method: "POST",
+				headers: { 'content-type': 'application/json' },
+				body: JSON.stringify(regobj)
+			}).then((res) => {
+				toast.success('Logged In Succesfully.')
+				navigate('/login')
+			}).catch((err) => {
+				toast.error('Failed : ' + err.message);
+			});
+
+		}
+
+	}	// const ProceedLogin = (e) => {
+	// 	e.preventDefault()
+	// 	let regobj = { name, surname, email, bio, dateofbirth, position, contact}
+	// 	if (validate()) {
+	// 		fetch('http://localhost:5000/user/' + name).the((res)=>{
+	// 			return res.json();
+	// 		}).then((resp)=>{
+	// 			console.log(resp)
+	// 		}).catch((err)=>{
+	// 			toast.error('Login')
+
+	// 		});
+	// 		// console.log('success')
+
+	// 	}
+
+	// }
 	return (
 		<div className="row">
 			<div className="offset-lg-3 col-lg-6">
-				<form onSubmit={ProceedLogin} className="container">
+				<form className="container" onSubmit={handlesubmit}>
 					<div className="card">
 						<div className="card-header">
 							<h2>User Login</h2>
@@ -55,7 +123,7 @@ const Login = () => {
 						<div className="card-body">
 							<label>Name <span className="errmsg ">*</span></label>
 							<div className="form-group">
-								<input value={name} onChange={e => nameupdate(e.target.value)} className="form-control"></input>
+								<input type="name" value={name} onChange={e => nameupdate(e.target.value)} className="form-control"></input>
 							</div>
 							<div className="form-group">
 								<label>Surname<span className="errmsg "> *</span></label>
